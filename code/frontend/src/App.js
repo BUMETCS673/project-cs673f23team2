9525197d1a763252ed50d02215717607817722cb
Siddhesh Dighe <siddheshdighe17@gmail.com>
2023-10-26 01:08:44 -0400

    SE-37: Fixing merge conflicts

--- code/frontend/src/App.js
+++ code/frontend/src/App.js
@@ -1,14 +1,9 @@
 import './styles/App.css';
 import React, { Component }  from 'react';
-<<<<<<< HEAD
 import {Route, Routes, Link} from 'react-router-dom';
 import {ReactComponent as Logo} from './assets/logo_with_text.svg';
 import Search from './components/Search';
 import EducationalVideoFeed from './components/EducationalVideoFeed';
-=======
-import {Route, Routes} from 'react-router-dom';
-import {ReactComponent as Logo} from './assets/logo_with_text.svg';
->>>>>>> 17f3921fe149f02dbe890c8bf3d38b968bcef9ab
 
 function App() {
 
@@ -16,19 +11,12 @@ function App() {
     return(
       <div className="AppSignInContainerWrapper">
           <div className="AppSignInLogoContainerWrapper">
-<<<<<<< HEAD
             <Logo data-cy="appBanner" className="ApplicationLogo"/>
           </div>
           <div className="AppSignInButtonContainerWrapper">
             <Link to="/search">
               <button data-cy="signInButton">Sign-In with Google</button>
             </Link>
-=======
-            <Logo className="ApplicationLogo"/>
-          </div>
-          <div className="AppSignInButtonContainerWrapper">
-            <button>Sign-In with Google</button>
->>>>>>> 17f3921fe149f02dbe890c8bf3d38b968bcef9ab
           </div>
         </div>
     )
@@ -38,11 +26,8 @@ function App() {
     <div className="App">
       <Routes>
         <Route path='/' element={screenToShow()} />
-<<<<<<< HEAD
         <Route path='/search' element={<Search />} />
         <Route path='/browse' element={<EducationalVideoFeed />} />
-=======
->>>>>>> 17f3921fe149f02dbe890c8bf3d38b968bcef9ab
       </Routes>
     </div>
   );
