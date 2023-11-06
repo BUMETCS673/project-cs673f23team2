b00f7fe604a0ac6faabe7e3a871bcd5cffe84f77
Paridhi Talwar <88367084+paridhitalwar@users.noreply.github.com>
2023-11-05 21:31:18 -0500

    SE-30: Create watch video page

---
+++ code/frontend/src/components/VideoDetails.js
@@ -0,0 +1,8 @@
+// Get the reward points and name elements
+const rewardPointsElement = document.querySelector('#reward-points');
+const nameElement = document.querySelector('#name');
+
+// Update the reward points and name elements
+rewardPointsElement.textContent = 100;
+nameElement.textContent = 'Paridhi Talwar';
+
