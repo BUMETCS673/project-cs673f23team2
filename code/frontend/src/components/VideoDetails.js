a7b8fa9de1e30ce93407c85332302ef7babd9a1c
Paridhi Talwar <88367084+paridhitalwar@users.noreply.github.com>
2023-11-07 12:12:40 -0500

    SE-30: Create watch video page

--- code/frontend/src/components/VideoDetails.js
+++ code/frontend/src/components/VideoDetails.js
@@ -1,8 +1,37 @@
-// Get the reward points and name elements
-const rewardPointsElement = document.querySelector('#reward-points');
-const nameElement = document.querySelector('#name');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../styles/VideoDetails.css'
 
-// Update the reward points and name elements
-rewardPointsElement.textContent = 100;
-nameElement.textContent = 'Paridhi Talwar';
+const VideoDetails = () => {
+  return (
+    <div className="container">
+      <button type="button">
+        <img src="logo.png" alt="Logo" />
+      </button>
+      <div className="profile-card">
+        <img src="profile-pic.jpg" alt="Profile picture" />
+        <h4>Reward Points: <span id="reward-points">100</span></h4>
+      </div>
+      <div className="video-player">
+        <h1>Video Player</h1>
+        <iframe
+                width="420"
+                height="315"
+                src="https://www.youtube.com/embed/SqcY0GlETPk"
+                frameBorder="0"
+                allowFullScreen
+              ></iframe>
+        <div className="controls">
+        <div class="video-details">
+            <h2 id="video-title">React Tutorial for Beginners</h2>
+            <p id="video-owner">Programming with Mosh</p>
+        </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
+ReactDOM.render(<VideoDetails />, document.getElementById('root'));
+
+export default App;
