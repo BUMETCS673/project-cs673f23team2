d73f131235877dedc727608b4feae84175bb7752
Ayushpurbia <55231420+Ayushpurbia@users.noreply.github.com>
2023-11-09 01:56:32 -0500

    SE-38: Create user onboarding page (#28)
    Co-authored-by: deepalii17 <145170779+deepalii17@users.noreply.github.com>

--- code/frontend/src/App.js
+++ code/frontend/src/App.js
@@ -1,29 +1,27 @@
 import './styles/App.css';
-import React, { Component }  from 'react';
-import {Route, Routes, Link} from 'react-router-dom';
-import {ReactComponent as Logo} from './assets/logo_with_text.svg';
+import React from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
+import { ReactComponent as Logo } from './assets/logo_with_text.svg';
 import Search from './components/Search';
 import EducationalVideoFeed from './components/EducationalVideoFeed';
 import EntertainmentFeed from './components/EntertainmentFeed';
 import SeeAllVideos from './components/SeeAllVideos'
 import UserProfile from './components/UserProfile';
 import VideoHistory from './components/VideoHistory';
+import OnboardingPage from './OnboardingPage';
 
 function App() {
-
   const screenToShow = () => {
-    return(
+    return (
       <div className="AppSignInContainerWrapper">
-          <div className="AppSignInLogoContainerWrapper">
-            <Logo data-cy="appBanner" className="ApplicationLogo"/>
-          </div>
-          <div className="AppSignInButtonContainerWrapper">
-            <Link to="/search">
-              <button data-cy="signInButton">Sign-In with Google</button>
-            </Link>
-          </div>
+        <div className="AppSignInLogoContainerWrapper">
+          <Logo data-cy="appBanner" className="ApplicationLogo" />
         </div>
-    )
+        <div className="AppSignInButtonContainerWrapper">
+          <Link to="/onboarding">Go to Onboarding Page</Link>
+        </div>
+      </div>
+    );
   }
 
   return (
@@ -36,6 +34,7 @@ function App() {
         <Route path='/all' element={<SeeAllVideos />} />
         <Route path='/users/:userId' element={<UserProfile />} />
         <Route path='/users/:userId/video-history' element={<VideoHistory />} />
+        <Route path='/onboarding' element={<OnboardingPage />} />
       </Routes>
     </div>
   );
---
+++ code/frontend/src/components/OnboardingPage.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import '../styles/onboarding.css'; // Updated path to the CSS file
+
+const OnboardingPage = () => {
+  const [hobbies, setHobbies] = useState([]);
+  const [hobbyInput, setHobbyInput] = useState("");
+
+  // Function to handle the input change
+  const handleInputChange = (event) => {
+    setHobbyInput(event.target.value);
+  };
+
+  // Function to add hobby from input box
+  const addHobby = () => {
+    if (hobbyInput && !hobbies.includes(hobbyInput)) {
+      setHobbies([...hobbies, hobbyInput]);
+      setHobbyInput('');
+    }
+  };
+
+  // Function to add hobby from suggestions (converted to lowercase)
+  const addSuggestedHobby = (suggestedHobby) => {
+    const suggestedHobbyLowerCase = suggestedHobby.toLowerCase();
+
+    if (!hobbies.includes(suggestedHobbyLowerCase)) {
+      setHobbies([...hobbies, suggestedHobbyLowerCase]);
+    }
+  };
+
+  // Function to remove a hobby
+  const removeHobby = (hobbyToRemove) => {
+    setHobbies(hobbies.filter(hobby => hobby !== hobbyToRemove));
+  };
+
+  return (
+    <div className="onboarding-container">
+      <h2>User Onboarding</h2>
+      <div className="profile-section">
+        {/* Placeholder for the user profile picture */}
+        <div className="profile-pic-placeholder"></div>
+        <p>Tell us about your Hobbies</p>
+        <div className="hobbies-input">
+          <input
+            type="text"
+            value={hobbyInput}
+            onChange={handleInputChange}
+            placeholder="Enter your hobby"
+          />
+          <button onClick={addHobby}>Add</button>
+        </div>
+        <div className="hobbies-suggestions">
+          {['Singing', 'Guitar', 'Dancing', 'Cooking'].map((suggestion) => (
+            <button key={suggestion} onClick={() => addSuggestedHobby(suggestion)}>
+              {suggestion}
+            </button>
+          ))}
+        </div>
+      </div>
+      <div className="hobbies-list">
+        {hobbies.map((hobby, index) => (
+          <div key={index} className="hobby-tag">
+            {hobby}
+            <button onClick={() => removeHobby(hobby)}>x</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default OnboardingPage;
