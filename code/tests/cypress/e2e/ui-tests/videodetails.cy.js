058604705ace770a51f9a5812e754ac3a8f1c87c
Paridhi Talwar <88367084+paridhitalwar@users.noreply.github.com>
2023-11-11 01:02:47 -0500

    SE-44: Unit tests for VideoDetails

---
+++ code/tests/cypress/e2e/ui-tests/videodetails.cy.js
@@ -0,0 +1,22 @@
+//SE - 30
+describe('Clicking on a video results in opening the video details for that video', () => {
+
+	beforeEach(() => {
+		cy.visit('http://localhost:3000/search')
+		cy.get('[data-cy=searchBarElement]').type('Flask tutorial')
+		cy.get('[data-cy=searchBarButton]').click();
+		cy.get('[data-cy=videoLink0]').click();
+	});
+
+    it('Video Player should be visible', () => {
+        cy.get('[data-cy=videoElement]').should('exist');
+    })
+
+    it('A label indicating video title', () => {
+        cy.get('[data-cy=videoTitleElement]').should('exist');
+    })
+
+    it('A label indicating video creator', () => {
+        cy.get('[data-cy=videoCreatorElement]').should('exist');
+    })
+});
