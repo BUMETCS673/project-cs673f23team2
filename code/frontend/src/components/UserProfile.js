7026ee1c6a3e4dee4df4096d85bc14a481b7232d
uppala-supriya <81250843+uppala-supriya@users.noreply.github.com>
2023-12-04 15:29:12 -0500

    Update UserProfile.js
    added code to fetch the data from database

--- code/frontend/src/components/UserProfile.js
+++ code/frontend/src/components/UserProfile.js
@@ -1,16 +1,49 @@
-import React, { useState } from 'react';
-//import '../styles/UserProfile.css';;
+import React, { useState, useLayoutEffect } from 'react';
+import { getAuth } from 'firebase/auth';
+import axios from 'axios';
+import '../styles/UserProfile.css'
+
+
 
 function UserProfile() {
+  const [hobbies, setHobbies] = useState([]);
+  const [videoUrls, setVideoUrls] = useState([]);
+  const [userProfile, setUserProfile] = useState({});
   
-  const [hobbies, setHobbies] = useState(['Singing', 'Dancing', 'Tennis']);
-  const [videoUrls, setVideoUrls] = useState([
-    'https://www.youtube.com/embed/Z1RJmh_OqeA',
-    'https://www.youtube.com/embed/pTFZFxd4hOI',
-    'https://www.youtube.com/embed/xk4_1vDrzzo',
-  ]);
 
-  
+  useLayoutEffect(() => {
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    if (user !== null && user !== undefined) {
+      // Fetch user hobbies
+      axios.get("http://127.0.0.1:5000/getUserHobbies", {
+        params: { userId: user.uid }
+      })
+        .then((response) => {
+          setHobbies(response.data.hobbies);  
+        })
+        .catch((error) => {
+          setHobbies(['hiking', 'reading', 'photography']);  
+        });
+
+      // Fetch user profile
+      axios.get("http://127.0.0.1:5000/getUserProfile", {
+        params: { userId: user.uid }
+      })
+        .then((response) => {
+          setUserProfile(response.data.profile);  
+        })
+        .catch((error) => {
+          // Handle error or set a default profile
+          setUserProfile({});  
+        });
+    } else {
+      // Set default values if user is not logged in
+      setHobbies(['hiking', 'reading', 'photography']);  
+      setUserProfile({});
+    }
+  }, []);
 
   return (
     <div>
@@ -33,8 +66,7 @@ function UserProfile() {
                 data-cy='ProfilePictureElement'
                 src="https://www.shareicon.net/data/256x256/2016/09/15/829472_man_512x512.png"
                 alt="Profile Image"
-              />{' '}
-              {/* Replace with your image */}
+              />
             </div>
             <span data-cy='ProfileNameElement'>Supriya Uppala</span>
           </div>
@@ -48,7 +80,7 @@ function UserProfile() {
           ))}
         </section>
 
-        <section data-cy='VideoHistoryElement'className="video-history">
+        <section data-cy='VideoHistoryElement' className="video-history">
           <h2>Video History</h2>
           <div className="videos">
             {videoUrls.map((videoUrl, index) => (
@@ -61,7 +93,7 @@ function UserProfile() {
                 allowFullScreen
               ></iframe>
             ))}
-            <button data-cy='ViewHistoryButton' >See All</button>
+            <button data-cy='ViewHistoryButton'>See All</button>
           </div>
         </section>
 
