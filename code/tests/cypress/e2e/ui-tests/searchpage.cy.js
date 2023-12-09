08d2162b66fd2a41534c87baca25f9c09410e8a6
Paridhi Talwar <88367084+paridhitalwar@users.noreply.github.com>
2023-12-09 11:56:15 -0500

    SE:52: UI test for Search bar component

--- code/tests/cypress/e2e/ui-tests/searchpage.cy.js
+++ code/tests/cypress/e2e/ui-tests/searchpage.cy.js
@@ -25,21 +25,4 @@ describe('As a user, I want to a search videos page so that I can search for top
         cy.get('[data-cy=searchBarElement]').should('exist');
     });
 
-    it('5. A "Search" button to search for the mentioned topic ', () => {
-      cy.get('[data-cy=searchBarButton]').should('exist');
-    });
-
-    //TODO: To add 6 and 6.1 Acceptance test for SE-27
-
-    it('7. Clicking on the search button should navigate the user to the "Browse Videos" page with the mentioned topic"', () => {
-      cy.get('[data-cy=searchBarElement]').type('Flask tutorial');
-      cy.get('[data-cy=searchBarButton]').click();
-      cy.url().should('include', '/browse');
-    });
-
-    it('7.1 Clicking on the search button should do nothing if the textbox to enter the topic is empty.', () => {
-      cy.get('[data-cy=searchBarButton]').click();
-      cy.location('pathname').should('eq', '/search');
-    });
-
   });
