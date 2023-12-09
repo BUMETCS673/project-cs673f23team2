d1226a68ea987416246ae64352cd1cb71293c6a1
Ayushpurbia <ayushpurbia9@gmail.com>
2023-12-09 18:40:15 -0500

    SE-7: USER PROFILE

--- code/frontend/src/components/UserProfile.js
+++ code/frontend/src/components/UserProfile.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { getAuth } from 'firebase/auth';
 import axios from 'axios';
-import '../styles/UserProfile.css'
+import '../styles/UserProfile.css';
 
 function UserProfile() {
   const [name, setName] = useState('');
@@ -14,32 +14,32 @@ function UserProfile() {
 
     if (user) {
       axios.get("http://127.0.0.1:5000/getUserProfile", { params: { userId: user.uid }})
-      .then((result) => {
-        console.log(result.data.userDetails);
-        setName(result.data.userDetails.name);
-        setProfile(result.data.userDetails.profile);
-        setHobbies(result.data.userDetails.hobbies);
-     }).catch((error) => {
-        console.log(error);
-      });
+        .then((result) => {
+          setName(result.data.userDetails.name);
+          setProfile(result.data.userDetails.profile);
+          setHobbies(result.data.userDetails.hobbies);
+        }).catch((error) => {
+          console.error(error);
+        });
     }
   }, []);
 
   return (
     <div className="user-profile-container">
-      <div className="profile-header">
-        <img src={profile} alt={`${name}`} className="profile-avatar" />
-        <h1 className="profile-name">{name}</h1>
+      <div className="user-profile-header">
+        <div className="user-profile-image-container">
+          <img src={profile} alt={`${name}`} className="user-profile-picture" />
+        </div>
+        <h1 className="user-profile-name">{name}</h1>
       </div>
-      <div className="profile-hobbies">
+      <div className="user-profile-hobbies">
         <h2>Hobbies:</h2>
-        <div className="hobbies-list">
+        <div className="user-hobbies-list">
           {hobbies.map((hobby, index) => (
-            <button key={index} className="hobby-button">{hobby}</button>
+            <span key={index} className="user-hobby">{hobby}</span>
           ))}
         </div>
       </div>
-      {/* Assuming Video History and buttons below are implemented elsewhere */}
     </div>
   );
 }
