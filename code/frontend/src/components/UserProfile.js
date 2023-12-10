b4762527b43d28f1be858fab99b7a145e157efb3
deepalii17 <145170779+deepalii17@users.noreply.github.com>
2023-12-09 19:54:08 -0500

    SE-7: User Profile (#59)
    Co-authored-by: uppala-supriya <81250843+uppala-supriya@users.noreply.github.com>
    Co-authored-by: Ayushpurbia <ayushpurbia9@gmail.com>

--- code/frontend/src/components/UserProfile.js
+++ code/frontend/src/components/UserProfile.js
@@ -1,77 +1,45 @@
-import React, { useState } from 'react';
-//import '../styles/UserProfile.css';;
+import React, { useState, useEffect } from 'react';
+import { getAuth } from 'firebase/auth';
+import axios from 'axios';
+import '../styles/UserProfile.css';
 
 function UserProfile() {
-  
-  const [hobbies, setHobbies] = useState(['Singing', 'Dancing', 'Tennis']);
-  const [videoUrls, setVideoUrls] = useState([
-    'https://www.youtube.com/embed/Z1RJmh_OqeA',
-    'https://www.youtube.com/embed/pTFZFxd4hOI',
-    'https://www.youtube.com/embed/xk4_1vDrzzo',
-  ]);
+  const [name, setName] = useState('');
+  const [profile, setProfile] = useState('');
+  const [hobbies, setHobbies] = useState([]);
 
-  
+  useEffect(() => {
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    if (user) {
+      axios.get("http://127.0.0.1:5000/getUserProfile", { params: { userId: user.uid }})
+        .then((result) => {
+          setName(result.data.userDetails.name);
+          setProfile(result.data.userDetails.profile);
+          setHobbies(result.data.userDetails.hobbies);
+        }).catch((error) => {
+          console.error(error);
+        });
+    }
+  }, []);
 
   return (
-    <div>
-      <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>User Profile</title>
-      </head>
-      <body>
-        <header>
-          {/* Home Button */}
-          <div className="home-container">
-            <button id="home">🔄 Home</button>
-          </div>
-
-          {/* User Profile Container */}
-          <div className="profile-container">
-            <div className="profile-pic">
-              <img
-                data-cy='ProfilePictureElement'
-                src="https://www.shareicon.net/data/256x256/2016/09/15/829472_man_512x512.png"
-                alt="Profile Image"
-              />{' '}
-              {/* Replace with your image */}
-            </div>
-            <span data-cy='ProfileNameElement'>Supriya Uppala</span>
-          </div>
-          <span></span> {/* Placeholder element */}
-        </header>
-
-        <section data-cy='HobbiesElement' className="hobbies">
-          <h2>Hobbies:</h2>
+    <div className="user-profile-container">
+      <div className="user-profile-header">
+        <div className="user-profile-image-container">
+          <img src={profile} alt={`${name}`} className="user-profile-picture" />
+        </div>
+        <h1 className="user-profile-name">{name}</h1>
+      </div>
+      <div className="user-profile-hobbies">
+        <h2>Hobbies:</h2>
+        <div className="user-hobbies-list">
           {hobbies.map((hobby, index) => (
-            <button key={index}>{hobby}</button>
+            <span key={index} className="user-hobby">{hobby}</span>
           ))}
-        </section>
-
-        <section data-cy='VideoHistoryElement'className="video-history">
-          <h2>Video History</h2>
-          <div className="videos">
-            {videoUrls.map((videoUrl, index) => (
-              <iframe
-                key={index}
-                width="300"
-                height="300"
-                src={videoUrl}
-                frameBorder="0"
-                allowFullScreen
-              ></iframe>
-            ))}
-            <button data-cy='ViewHistoryButton' >See All</button>
-          </div>
-        </section>
-
-        <footer>
-          {/* Footer Buttons */}
-          <button>Clear History</button>
-          <button>Delete Profile</button>
-          <button>Logout</button>
-        </footer>
-      </body>
+        </div>
+      </div>
     </div>
   );
 }
