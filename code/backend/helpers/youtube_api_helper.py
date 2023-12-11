ec34beb4e48b45517fb37a65831fab9ef344fc2c
uppala-supriya <81250843+uppala-supriya@users.noreply.github.com>
2023-12-10 19:02:02 -0500

    SE-72: VideoDetails - comments and refactoring code (#90)

--- code/backend/helpers/youtube_api_helper.py
+++ code/backend/helpers/youtube_api_helper.py
@@ -3,7 +3,7 @@ from isodate import parse_duration
 
 video_fetch_url = "https://www.googleapis.com/youtube/v3/search"
 video_data_fetch_url = "https://www.googleapis.com/youtube/v3/videos"
-key = "AIzaSyCqn_iHs55mJrxYNSCNLNFQtnSzT9UYExM"  # Kept here for testing, will be moved to an environment variable
+key = "AIzaSyCmfbtelCSnTCkYyI0ZyAUSr2-mEtq65XQ"  # Kept here for testing, will be moved to an environment variable
 
 
 # Create parameters for the Youtube Search API Request
